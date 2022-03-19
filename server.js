19f7a52cee5472c22f323f91f7591756e766f15d
Lina <woorin.lee1524@gmail.com>
2022-03-18 19:19:35 -0700

    added server.js

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('RECEIVED GET REQUEST!')
+});
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+})
+
+
+
+
+
+
+
+
+
+
+
+
