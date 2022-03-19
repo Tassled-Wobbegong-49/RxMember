01e438b9c6139c103c5b115e759fbdab2aab2bd7
William Chu <97659282+wi11chu@users.noreply.github.com>
2022-03-18 19:04:59 -0700

    Initial Files 03.18.22 - 7PM

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: path.join(__dirname, "client", "index.js"),
+
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+
+  mode: 'development',
+
+  devServer: { 
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+      publicPath: '/dist',
+    },
+    proxy: {
+      '/': 'http://localhost:3000'
+    },
+    compress: true,
+    port: 8080,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+       },
+      },
+      {
+        test: /\.(css)$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "client", "index.html"),
+      inject: "body"
+    }),
+  ],
+
+
+
+}
